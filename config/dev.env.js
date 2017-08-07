8dd3d758066ac88cd4ffbfcd6a1264aa8e3f1946
John Datserakis <johndatserakis@gmail.com>
2017-08-07 00:01:33 -0400

    initial clean up to new flow

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"localhost:4000"',
+  API_URL: '"http://localhost:4000"',
 })
--- src/store/index.js
+++ src/store/index.js
@@ -9,84 +9,52 @@ axios.defaults.baseURL = process.env.API_URL;
 
 import { addAuthorizationHeader, cleanAuthorizationResponse } from '@/common/utilities'
 
+import jwtDecode from 'jwt-decode';
+
 //Mutations
-const LOGOUT_USER = 'LOGOUT_USER'
-const STORE_ACCESS_TOKEN = 'STORE_ACCESS_TOKEN'
-const SET_AUTHENTICATED = 'SET_AUTHENTICATED'
-const SET_AUTHENTICATED_USER = 'SET_AUTHENTICATED_USER'
+// const LOGOUT_USER = 'LOGOUT_USER'
+// const STORE_ACCESS_TOKEN = 'STORE_ACCESS_TOKEN'
+// const SET_AUTHENTICATED = 'SET_AUTHENTICATED'
+// const SET_AUTHENTICATED_USER = 'SET_AUTHENTICATED_USER'
+
+const SET_USER = 'SET_USER'
 
 const store = new Vuex.Store({
     state: {
-        authenticated: false,
+        // authenticated: false,
         user: false,
     },
     mutations: {
-        LOGOUT_USER(state, token) {
-            localStorage.removeItem('accessToken')
-            state.user = false
-            state.authenticated = false
-        },
-        STORE_ACCESS_TOKEN(state, token) {
-            localStorage.setItem('accessToken', cleanAuthorizationResponse(token))
-        },
-        SET_AUTHENTICATED_USER(state, user) {
-            state.user = user
-        },
-        SET_AUTHENTICATED(state, boolean) {
-            state.authenticated = true
-        },
+        SET_USER(state, data) {
+            state.user = data
+        }
+
+
+
+        // LOGOUT_USER(state, token) {
+        //     localStorage.removeItem('accessToken')
+        //     state.user = false
+        //     state.authenticated = false
+        // },
+        // STORE_ACCESS_TOKEN(state, token) {
+        //     localStorage.setItem('accessToken', cleanAuthorizationResponse(token))
+        // },
+        // SET_AUTHENTICATED_USER(state, user) {
+        //     state.user = user
+        // },
+        // SET_AUTHENTICATED(state, boolean) {
+        //     state.authenticated = true
+        // },
     },
     getters: {
-        authenticated (state) {
-            return state.authenticated
-        },
+        // authenticated (state) {
+        //     return state.authenticated
+        // },
         user (state) {
             return state.user
         },
     },
     actions: {
-        getAuthenticatedUser({commit}){
-            return new Promise((resolve, reject) => {
-                console.log(localStorage.getItem("accessToken"))
-                if (localStorage.getItem("accessToken") === null) {
-                    return reject('Error')
-                }
-
-                addAuthorizationHeader(localStorage.getItem("accessToken"))
-                axios.post('/api/v1/user/getAuthenticatedUser', null)
-                .then(response => {
-                    commit(STORE_ACCESS_TOKEN, response.headers.authorization)
-                    commit(SET_AUTHENTICATED_USER, response.data.user)
-                    commit(SET_AUTHENTICATED, true)
-                    return resolve()
-                })
-                .catch(error => {
-                    return reject('Error')
-                })
-            })
-        },
-
-        userSignup({commit}, credentials){
-            return new Promise((resolve, reject) => {
-                axios.post('/api/v1/user/signup', {
-                    first_name: credentials.first_name,
-                    last_name: credentials.last_name,
-                    username: credentials.username,
-                    email: credentials.email,
-                    password: credentials.password
-                })
-                .then(response => {
-                    Vue.toasted.success('Successfully signed up. Please login.')
-                    resolve(response)
-                })
-                .catch(error => {
-                    for (var key in error.response.data.messages) {
-                        Vue.toasted.error(error.response.data.messages[key]);
-                    }
-                    return reject(error)
-                })
-            })
-        },
         userLogin({commit}, credentials){
             return new Promise((resolve, reject) => {
                 axios.post('/api/v1/user/authenticate', {
@@ -94,99 +62,169 @@ const store = new Vuex.Store({
                     password: credentials.password
                 })
                 .then(response => {
-                    commit(STORE_ACCESS_TOKEN, response.data.data.token)
+                    let decoded = jwtDecode(response.data.access_token)
+                    commit(SET_USER, decoded.data[0])
                     Vue.toasted.success('Successfully logged in.')
-                    resolve(response)
+                    return resolve()
                 })
                 .catch(error => {
                     console.log(error)
-                    for (var key in error.response.data.messages) {
-                        Vue.toasted.error(error.response.data.messages[key]);
-                    }
-                    return reject(error)
-                })
-            })
-        },
-        logout({commit}){
-            return new Promise((resolve, reject) => {
-                commit(LOGOUT_USER)
-                resolve()
-            })
-        },
-        userForgot({commit}, credentials){
-            return new Promise((resolve, reject) => {
-                axios.post('/api/v1/user/forgot', {
-                    email: credentials.email,
-                })
-                .then(response => {
-                    credentials.token = response.data.data.token
-                    store.dispatch('sendForgotEmail', credentials)
-                    resolve()
-                })
-                .catch(error => {
-                    resolve()
-                })
-            })
-        },
-        sendForgotEmail({commit}, credentials){
-            return new Promise((resolve, reject) => {
-                axios.post('/api/v1/user/sendForgotEmail', {
-                    email: credentials.email,
-                    token: credentials.token
-                })
-                .then(response => {
-                    Vue.toasted.success('Please check your email address.')
-                    resolve()
-                })
-                .catch(error => {
-                    Vue.toasted.success('Please check your email address.')
-                    resolve()
-                })
-            })
-        },
-        checkResetToken({commit}, token) {
-            return new Promise((resolve, reject) => {
-                axios.post('/api/v1/user/checkResetToken', {
-                    token: token
-                })
-                .then(response => {
-                    resolve(response.data.data.email)
-                })
-                .catch(error => {
-                    Vue.toasted.error('Your link has expired or is invalid. Please reset your password again.');
-                    return reject(error)
-                })
-            })
-        },
-        userReset({commit}, credentials){
-            return new Promise((resolve, reject) => {
-                axios.post('/api/v1/user/reset', {
-                    password: credentials.password,
-                    token: credentials.token,
-                    email: credentials.email,
-                })
-                .then(response => {
-                    Vue.toasted.success('Successfully reset password. Please login.')
-                    resolve(response)
-                })
-                .catch(error => {
-                    Vue.toasted.error('Your reset link has expired or in incorrect. Please reset your password again.');
-                    return reject()
+                    // Vue.toasted.error('Hmm, that does\'t seem to match.')
+                    // console.log(error)
+                    // for (var key in error.response.data.messages) {
+                    //     Vue.toasted.error(error.response.data.messages[key]);
+                    // }
+                    // return reject(error)
                 })
             })
         },
 
-        getUsersNotes({commit}){
-            return new Promise((resolve, reject) => {
-                addAuthorizationHeader(localStorage.getItem("accessToken"))
-                axios.post('/api/v1/notes', null)
-                .then(response => {
-                    commit(STORE_ACCESS_TOKEN, response.headers.authorization)
-                    resolve(response.data)
-                })
-                .catch(error => {})
-            })
-        },
+
+
+
+
+        // getAuthenticatedUser({commit}){
+        //     return new Promise((resolve, reject) => {
+        //         console.log(localStorage.getItem("accessToken"))
+        //         if (localStorage.getItem("accessToken") === null) {
+        //             return reject('Error')
+        //         }
+
+        //         addAuthorizationHeader(localStorage.getItem("accessToken"))
+        //         axios.post('/api/v1/user/getAuthenticatedUser', null)
+        //         .then(response => {
+        //             commit(STORE_ACCESS_TOKEN, response.headers.authorization)
+        //             commit(SET_AUTHENTICATED_USER, response.data.user)
+        //             commit(SET_AUTHENTICATED, true)
+        //             return resolve()
+        //         })
+        //         .catch(error => {
+        //             return reject('Error')
+        //         })
+        //     })
+        // },
+
+        // userSignup({commit}, credentials){
+        //     return new Promise((resolve, reject) => {
+        //         axios.post('/api/v1/user/signup', {
+        //             first_name: credentials.first_name,
+        //             last_name: credentials.last_name,
+        //             username: credentials.username,
+        //             email: credentials.email,
+        //             password: credentials.password
+        //         })
+        //         .then(response => {
+        //             Vue.toasted.success('Successfully signed up. Please login.')
+        //             resolve(response)
+        //         })
+        //         .catch(error => {
+        //             for (var key in error.response.data.messages) {
+        //                 Vue.toasted.error(error.response.data.messages[key]);
+        //             }
+        //             return reject(error)
+        //         })
+        //     })
+        // },
+        // userLogin({commit}, credentials){
+        //     return new Promise((resolve, reject) => {
+        //         axios.post('/api/v1/user/authenticate', {
+        //             username: credentials.username,
+        //             password: credentials.password
+        //         })
+        //         .then(response => {
+        //             commit(STORE_ACCESS_TOKEN, response.data.data.token)
+        //             Vue.toasted.success('Successfully logged in.')
+        //             resolve(response)
+        //         })
+        //         .catch(error => {
+        //             console.log(error)
+        //             for (var key in error.response.data.messages) {
+        //                 Vue.toasted.error(error.response.data.messages[key]);
+        //             }
+        //             return reject(error)
+        //         })
+        //     })
+        // },
+        // logout({commit}){
+        //     return new Promise((resolve, reject) => {
+        //         commit(LOGOUT_USER)
+        //         resolve()
+        //     })
+        // },
+        // userForgot({commit}, credentials){
+        //     return new Promise((resolve, reject) => {
+        //         axios.post('/api/v1/user/forgot', {
+        //             email: credentials.email,
+        //         })
+        //         .then(response => {
+        //             credentials.token = response.data.data.token
+        //             store.dispatch('sendForgotEmail', credentials)
+        //             resolve()
+        //         })
+        //         .catch(error => {
+        //             resolve()
+        //         })
+        //     })
+        // },
+        // sendForgotEmail({commit}, credentials){
+        //     return new Promise((resolve, reject) => {
+        //         axios.post('/api/v1/user/sendForgotEmail', {
+        //             email: credentials.email,
+        //             token: credentials.token
+        //         })
+        //         .then(response => {
+        //             Vue.toasted.success('Please check your email address.')
+        //             resolve()
+        //         })
+        //         .catch(error => {
+        //             Vue.toasted.success('Please check your email address.')
+        //             resolve()
+        //         })
+        //     })
+        // },
+        // checkResetToken({commit}, token) {
+        //     return new Promise((resolve, reject) => {
+        //         axios.post('/api/v1/user/checkResetToken', {
+        //             token: token
+        //         })
+        //         .then(response => {
+        //             resolve(response.data.data.email)
+        //         })
+        //         .catch(error => {
+        //             Vue.toasted.error('Your link has expired or is invalid. Please reset your password again.');
+        //             return reject(error)
+        //         })
+        //     })
+        // },
+        // userReset({commit}, credentials){
+        //     return new Promise((resolve, reject) => {
+        //         axios.post('/api/v1/user/reset', {
+        //             password: credentials.password,
+        //             token: credentials.token,
+        //             email: credentials.email,
+        //         })
+        //         .then(response => {
+        //             Vue.toasted.success('Successfully reset password. Please login.')
+        //             resolve(response)
+        //         })
+        //         .catch(error => {
+        //             Vue.toasted.error('Your reset link has expired or in incorrect. Please reset your password again.');
+        //             return reject()
+        //         })
+        //     })
+        // },
+
+        // getUsersNotes({commit}){
+        //     return new Promise((resolve, reject) => {
+        //         addAuthorizationHeader(localStorage.getItem("accessToken"))
+        //         axios.post('/api/v1/notes', null)
+        //         .then(response => {
+        //             commit(STORE_ACCESS_TOKEN, response.headers.authorization)
+        //             resolve(response.data)
+        //         })
+        //         .catch(error => {})
+        //     })
+        // },
     }
 })
 
