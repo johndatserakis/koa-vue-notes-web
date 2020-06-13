3eb9253451cae6f2d7c21839f16f5ea177a358c0
John Datserakis <johndatserakis@gmail.com>
2020-06-13 11:56:45 -0400

    Cleaning up some imports

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,6 @@ import App from "./App.vue";
 import router from "./router";
 // import store from "./store";
 
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-Vue.use(BootstrapVue);
-
 import VueProgressBar from "vue-progressbar";
 Vue.use(VueProgressBar, {
   color: "#ADE027",
